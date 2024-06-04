e0c14f3f14a9db3c173d5c7a56cb2dcf7159520a
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-06-04 10:24:35 +0700

    menambahkan statemnet if

--- StreamReaderandWriter_0166.cpp
+++ StreamReaderandWriter_0166.cpp
@@ -34,5 +34,8 @@ int main()
     infile.open("contohfile.txt");
 
     cout << endl << ">= Membuka dan membaca file " << endl;
-    
+    //jika file ada maka
+    if (infile.is_open()) {
+
+    }
 }
