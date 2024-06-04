760602209364f82859f3feb684141f23fee1f675
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-06-04 10:25:57 +0700

    menambahkan perulangan

--- StreamReaderandWriter_0166.cpp
+++ StreamReaderandWriter_0166.cpp
@@ -36,6 +36,10 @@ int main()
     cout << endl << ">= Membuka dan membaca file " << endl;
     //jika file ada maka
     if (infile.is_open()) {
-
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            
+        }
     }
 }
