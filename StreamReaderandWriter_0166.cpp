fbb291e7c6f802bd01b4743fbcf7b2a332a5407b
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-06-04 10:26:11 +0700

    menambahkan output pada perulangan

--- StreamReaderandWriter_0166.cpp
+++ StreamReaderandWriter_0166.cpp
@@ -39,7 +39,8 @@ int main()
         //melakukan perulangan setiap baris
         while (getline(infile, baris))
         {
-            
+            //dan tampilkan disini
+            cout << baris << '\n';
         }
     }
 }
