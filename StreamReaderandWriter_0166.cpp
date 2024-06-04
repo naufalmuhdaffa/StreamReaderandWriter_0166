f4a9345709898d9322da84ce435aeeae02418ae7
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-06-04 10:27:33 +0700

    menutup file

--- StreamReaderandWriter_0166.cpp
+++ StreamReaderandWriter_0166.cpp
@@ -42,5 +42,8 @@ int main()
             //dan tampilkan disini
             cout << baris << '\n';
         }
+        // tutup file tersebut setelah selesai
+        infile.close();
     }
+    
 }
